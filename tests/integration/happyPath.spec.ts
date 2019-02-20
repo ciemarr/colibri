0fffe9484fbed1de131979e09604a4f5e31985cb
Ciemarr Software <dev@ciemarr.com>
2019-02-19 23:31:15 -0800

    Update Cypress test for loading story from URL. [#164068437]

--- tests/integration/happyPath.spec.ts
+++ tests/integration/happyPath.spec.ts
@@ -1,28 +1,18 @@
 /// <reference types="Cypress" />
 
 context('Happy path', () => {
-  it('shows story text', () => {
-    cy.visit('http://localhost:8080/');
-
-    cy.get('.Story-title').contains('Gibberish');
-    cy.get('.Story-author').contains('J.S. Rando');
-    cy.get('.Story-text').should('exist');
-    cy.get('.Story-fin').should('exist');
-  });
-
-  /*
   it('loads a story from the URL', () => {
     cy.server();
-    cy.route(
-      'http://www.example.com/story/42',
-      {text: 'Hello, world!'}
-    ).as('externalStory');
 
-    cy.visit('http://localhost:8080/?url=http://www.example.com/story/42');
+    const storyUrl = 'http://www.example.com/story/42';
+    const storyText = 'Hello, world!';
+
+    cy.route(storyUrl, storyText).as('externalStory');
+
+    cy.visit(`http://localhost:8080/?url=${storyUrl}`);
 
     cy.wait('@externalStory');
 
-    cy.get('.Story-text').contains('Hello, world!');
+    cy.get('.Story-text').contains(storyText);
   });
-  */
 });
