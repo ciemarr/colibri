31a99f8ba77a1485953ac548938915a81345bb1b
Ciemarr Software <dev@ciemarr.com>
2019-02-20 23:59:10 -0800

    Add test coverage analysis to Coveralls.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') {
+      config.devtool('eval');
+    }
+
+    if (process.env.NODE_ENV === 'test') {
+      config.module
+        .rule('istanbul')
+          .test(/\.(js|ts|vue)$/)
+          .enforce('post')
+          .include
+            .add(path.resolve(__dirname, '/src'))
+            .end()
+          .use('istanbul-instrumenter-loader')
+            .loader('istanbul-instrumenter-loader')
+            .options({ esModules: true })
+            .end();
+    }
+  }
 }
