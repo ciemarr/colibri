12500ac46b91af03a3ae60ca0ec2758cd71c5272
Ciemarr Software <dev@ciemarr.com>
2019-02-21 10:24:50 -0800

    Add logger to Express server.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
+const logger = require('morgan');
 
 const port = process.env.PORT || 80;
 console.log('Colibri configured for port ' + port);
 
 const expressServer = express();
+
+expressServer.use(logger('dev'));
 
 const staticFileMiddleware = express.static('dist');
 expressServer.use(staticFileMiddleware); // 1st call for unredirected requests
